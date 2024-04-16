04084d9d352fd2c81d940301faebfb4484819347
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2024-04-16 15:35:50 +0530

    update app.jsx with use-effect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,55 @@ import './App.css'
 import ADD from './assets/ADD.jpg'
 import Subtract from './assets/Subtract.png'
 
-const VT = () =>{
-  return(
-    <>
-    <h1>JAVA JARGON</h1>
-    </>
-  );
-}
+// const VT = () =>{
+//   return(
+//     <>
+//     <h1>JAVA JARGON</h1>
+//     </>
+//   );
+// }
 
-const Pc = (props) => {
-  const {a,b} = props;
-  console.log(a+" "+b);
-  return (
-    <>
-        <div>{a+b}</div>
-    </>
-  );
-}
+// const Pc = (props) => {
+//   const {a,b} = props;
+//   console.log(a+" "+b);
+//   return (
+//     <>
+//         <div>{a+b}</div>
+//     </>
+//   );
+// }
 
-function App() {
-  const [counter,setCounter] = useState(0);
-  const [flag,setFlag] = useState(false);
-  useEffect( ()=>{setCounter(0);} , [flag]);
-  return (
-    <div class="container">
-      <img src= {ADD}
-        class = "img1" onClick={()=>{setCounter(counter+1)}}></img>
-      <div class="span">{counter}</div>
-      <img src= {Subtract}
-        class = "img2" onClick={()=>{setCounter(counter-1)}}></img>
-      <button class = "reset" onClick={()=>{setFlag(!flag)}}>Reset </button>
-    </div>
-  )
-}
+// function App() {
+//   const [counter,setCounter] = useState(0);
+//   const [flag,setFlag] = useState(false);
+//   useEffect( ()=>{setCounter(0);} , [flag]);
+//   return (
+//     <div class="container">
+//       <img src= {ADD}
+//         class = "img1" onClick={()=>{setCounter(counter+1)}}></img>
+//       <div class="span">{counter}</div>
+//       <img src= {Subtract}
+//         class = "img2" onClick={()=>{setCounter(counter-1)}}></img>
+//       <button class = "reset" onClick={()=>{setFlag(!flag)}}>Reset </button>
+//     </div>
+//   )
+// }
 
-const Darkmode = () => {
-  return (
-    <div>
-      darkmode
-    </div>
-  );
-}
+// const Darkmode = () => {
+//   return (
+//     <div>
+//       darkmode
+//     </div>
+//   );
+// }
 
-const Lightmode = () => {
-  return (
-    <div>
-      lightmode
-    </div>
-  );
-}
+// const Lightmode = () => {
+//   return (
+//     <div>
+//       lightmode
+//     </div>
+//   );
+// }
 
 // function App(){
 //   const [darkmode,setDarkmode] = useState(0);
@@ -74,4 +74,19 @@ const Lightmode = () => {
 //   );
 // }
 
+const App = () =>{
+  
+  const [count,updateCount] = useState(0);
+  useEffect(
+    ()=>{
+      alert("YES");
+    }
+  )
+  console.log("new"); 
+  return (
+    <div>
+      <button onClick={()=>updateCount(count+1)}>button</button>
+    </div>
+  );
+}
 export default App
